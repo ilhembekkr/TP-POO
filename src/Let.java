6e42b89ffae81852c8580171bbe1d50b12ea88ca
sarra_ben <js_bendaho@esi.dz>
2021-07-17 23:36:28 +0200

    KHRA

--- src/Let.java
+++ src/Let.java
@@ -3,71 +3,59 @@ import java.util.List;
 
 public class Let extends Commande {
 
-    private String variable ;
-    private String expression  ;
+    private String variable;
+    private String expression;
 
-    public Let (String op) throws DeclarationInterditeException{
+    public Let(String op) {
         super(op);
-        String variableInterdite[] = {"log", "l", "o", "s", "g", "lo", "og", "sin", "s", "i", "n", "si", "in", "cos", "c", "co", "os"};
-        String[] s=op.trim().split("=");
-        this.variable=s[0].trim();
-        boolean trouv = false ;
-        int i =0 ;
-        while ((trouv != true) && (  variableInterdite.length < i )){
+        // String variableInterdite[] = {"log", "l", "o", "s", "g", "lo", "og", "sin", "s", "i", "n", "si", "in", "cos", "c", "co", "os"};
+        String[] s = op.trim().split("=");
+        this.variable = s[0].trim();
+        boolean trouv = false;
+        int i = 0;
+       /* while ((trouv != true) && (  variableInterdite.length < i )){
             if (this.variable == variableInterdite[i] ) {
                 trouv = true ;
                 throw new DeclarationInterditeException() ;
             }
-        }
-        System.out.println(variable);
-        this.expression=s[1].trim();
-        System.out.println(expression);
+        }*/
+        //System.out.println(variable);
+        this.expression = s[1].trim();
+        //System.out.println(expression);
     }
 
 
-
-
-
-
-    public void executer(TableSymboles table)  {
+    public void executer(TableSymboles table) {
         Expression expVar = new Expression(expression);
-        boolean erreur=false ;
+        boolean erreur = false;
         //boolean isNum = false ;
-        double valeur=0 ;
-        if (expVar.isNumber()) {  valeur = Double.parseDouble(String.valueOf(expression)) ;}
-        else {
+        double valeur = 0;
+        if (expVar.isNumber()) {
+            valeur = Double.parseDouble(String.valueOf(expression));
+        } else {
             try {
                 expVar.analyserParent();
-                valeur  = expVar.evaluer(table);
+                valeur = expVar.evaluer(table);
 
 
             } catch (ParFermManqException e1) {
-                erreur=true ;
+                erreur = true;
                 System.out.println("parenthese fermante manquante !");
             } catch (ParOuvManqException e2) {
-                erreur=true ;
+                erreur = true;
                 System.out.println("parenthese ouvrante manquante !");
             }
         }
 
-        System.out.println(variable);
+        //System.out.println(variable);
 
-           if (!erreur ){ table.ajouterSymbole(variable,valeur);
+        if (!erreur) {
+            table.ajouterSymbole(variable, valeur);
+        }
 
-        System.out.println("valeur ajoutee"+valeur);}
-
-    }
-
-
-
-
-
-
-
-
-
-
+        //System.out.println("valeur ajoutee"+valeur);}
 
 
     }
+}
 
--- src/LigneDeCommande.java
+++ src/LigneDeCommande.java
@@ -5,16 +5,26 @@ public class LigneDeCommande {
         ligneStr = Cmd ;
     }
 
-    public  Commande extraireCommande() throws CommandeIntrouvableException,DeclarationInterditeException  {
-        String [] ligne =  ligneStr.split(" ");
+    public  Commande extraireCommande() throws CommandeIntrouvableException  {
+        //String [] ligne =  ligneStr.split(" ");
 
         if (ligneStr.trim().startsWith("let") ) {
             String operande = ligneStr.trim().replace("let","") ;
-            try {
-                return new Let(operande);
-            }catch(DeclarationInterditeException e){
-                System.out.println("Declaration interdite !");
+            String variableInterdite[] = {"log", "l", "o", "s", "g", "lo", "og", "sin", "s", "i", "n", "si", "in", "cos", "c", "co", "os","sqrt","t","q" , "sq","rt","qr","sqr","qrt","r"};
+            String[] s=operande.trim().split("=");
+            String variable = s[0].trim();
+            boolean trouv = false ;
+            int i =0 ;
+            while ((trouv == false ) && (  i < variableInterdite.length  )){
+                if (variable.compareTo(variableInterdite[i])==0 ) {
+                    trouv = true ;
+                    System.out.println("Declaration interdite !");
+                }
+                i++;
             }
+
+                return new Let(operande);
+
         }
         else if (ligneStr.trim().startsWith("print") ) {
             String operande = ligneStr.trim().replace("print","") ;
